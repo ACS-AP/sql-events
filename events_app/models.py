2f20172591a64e607d0413c252d481d793df8211
ArnavPag <arnav.pagidyala@students.dominican.edu>
2022-01-28 15:33:28 -0800

    added models and join

--- events_app/models.py
+++ events_app/models.py
@@ -11,6 +11,10 @@ from sqlalchemy.orm import backref
 
 class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    phone = db.Column(db.String(80), nullable=False)
+    event_relation = db.Column(db.Event)
 
 # TODO: Create a model called `Event` with the following fields:
 # - id: primary key
@@ -19,6 +23,12 @@ class Guest(db.Model):
 # - date_and_time: DateTime column
 # - guests: relationship to "Guest" table with a secondary table
 
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(80), nullable=False)
+    date_and_time = db.Column(db.String(80), nullable=False)
+    guest_relation = db.Column(db.Guest)
 # STRETCH CHALLENGE: Add a field `event_type` as an Enum column that denotes the
 # type of event (Party, Study, Networking, etc)
 
@@ -28,5 +38,10 @@ class Event(db.Model):
 # TODO: Create a table `guest_event_table` with the following columns:
 # - event_id: Integer column (foreign key)
 # - guest_id: Integer column (foreign key)
+
+guest_event_table = db.Table('event_guest',
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id')),
+    db.Column('guest_id', db.Integer, db.ForeignKey('guest.id'))
+)
 
 guest_event_table = None
